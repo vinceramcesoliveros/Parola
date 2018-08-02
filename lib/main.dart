a9ac8649cb0f04be8937881ed54a15c2a8c68948
ram231 <blusea231@gmail.com>
2018-08-02 10:47:17 +0800

    Fixed UI and some ERRORS

--- lib/main.dart
+++ lib/main.dart
@@ -1,126 +1,292 @@
 import 'dart:async';
 
+import 'package:final_parola/home.dart';
 import 'package:flutter/material.dart';
-import 'package:beacons/beacons.dart';
-import 'package:flutter_blue/flutter_blue.dart';
-import 'package:scoped_model/scoped_model.dart';
+// import 'package:beacons/beacons.dart';
+// import 'package:flutter_blue/flutter_blue.dart';
+// import 'package:scoped_model/scoped_model.dart';
 import 'package:simple_permissions/simple_permissions.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
-  runApp(MyApp());
-  StreamSubscription _scanSubscription;
-
-  Beacons.loggingEnabled = true;
-  Beacons.configure(BeaconsSettings(
-      android: BeaconsSettingsAndroid(logs: BeaconsSettingsAndroidLogs.info),
-      iOS: BeaconsSettingsIOS()));
+  runApp(SplashScreen());
 }
 
-class MyApp extends StatelessWidget {
+final FirebaseAuth auth = FirebaseAuth.instance;
+final GoogleSignIn signIn = new GoogleSignIn();
+bool loggedIn = false;
+
+///[Google Sign in API]
+Future<FirebaseUser> googleSignIn() async {
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  final GoogleSignInAccount googleUser = await signIn.signIn();
+  final GoogleSignInAuthentication googleAuth = await googleUser.authentication;
+  final FirebaseUser user = await auth.signInWithGoogle(
+      accessToken: googleAuth.accessToken, idToken: googleAuth.idToken);
+  assert(user.email != null);
+  assert(user.displayName != null);
+  assert(await user.getIdToken() != null);
+  final FirebaseUser currentUser = await auth.currentUser();
+//    SaveUser(
+//        username: user.displayName,
+//        useremail: user.email,
+//        userid: user.uid,
+//        userphotoURL: user.photoUrl);
+  prefs.setString("username", user.displayName);
+  prefs.setString("userid", user.uid);
+  prefs.setString("useremail", user.email);
+  prefs.setString("userphotoURL", user.photoUrl);
+  loggedIn = true;
+  return currentUser;
+}
+
+///This class will save the User's
+///information to access the homepage
+//class SaveUser {
+//  String username, useremail, userid, userphotoURL;
+//  SaveUser({this.username, this.useremail, this.userid, this.userphotoURL});
+//
+//  Future<Null> saveUser() async {
+//    SharedPreferences prefs = await SharedPreferences.getInstance();
+//    prefs.setString("user", this.username);
+//    prefs.setString("useremail", this.useremail);
+//    prefs.setString("userid", this.userid);
+//    prefs.setString("userphotoURL", this.userphotoURL);
+//  }
+//}
+
+class SplashScreen extends StatefulWidget {
+  @override
+  SplashScreenState createState() {
+    return new SplashScreenState();
+  }
+}
+
+class SplashScreenState extends State<SplashScreen> {
+  ///
+  ///Checkes if the [user] has signed in after closing
+  ///the application
+  Future<Null> _isLoggedIn() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    this.setState(() {
+      if (prefs.getString("username") != null) {
+        loggedIn = true;
+      } else {
+        loggedIn = false;
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(),
+      title: 'Parola',
+      // showPerformanceOverlay: true,
       debugShowCheckedModeBanner: false,
-      home: MyHomePage(),
+      home: HomePage(), //loggedIn == true ? HomePage() : LoginPage(),
+      initialRoute: "/login",
+      routes: {
+        '/login': (context) => LoginPage(),
+        '/home': (context) => HomePage(),
+      },
     );
+  }
+
+  @override
+  void initState() {
+    this._isLoggedIn();
+
+    // TODO: implement initState
+    super.initState();
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Parola"),
-      ),
-      body: BodyPage(),
+      body: PBodyPage(),
     );
   }
 }
 
-class BodyPage extends StatefulWidget {
+class PBodyPage extends StatefulWidget {
   @override
   _BodyPageState createState() => _BodyPageState();
 }
 
-class _BodyPageState extends State<BodyPage> {
+class _BodyPageState extends State<PBodyPage> {
+  ///Facebook Sign in Authentication
+  Future<FirebaseUser> _facebookSignIn() async {
+    return null;
+  }
+
+  ///Automatically Sign in from Splash Screen, this is unnecessary to our code
+  ///since We have a button to sign in through Google.
+//  @override
+//  void initSate() {
+//    super.initState();
+//    _auth.onAuthStateChanged.firstWhere((user) => user != null).then(
+//        (user) => Navigator.of(context).pushReplacementNamed('/homePage'));
+//    new Future.delayed(Duration(seconds: 1)).then((_) => _googleSignIn());
+//  }
+  static String lightHouse = 'assets/lighthouse.svg';
+  static Widget svg = SvgPicture.asset(
+    lightHouse,
+    color: Colors.red[400],
+    height: 128.0,
+    width: 104.0,
+  );
+  Widget parolaIcon = Stack(
+    children: <Widget>[
+      Padding(
+        padding: EdgeInsets.only(top: 64.0),
+        child: CircleAvatar(
+          maxRadius: 64.0,
+          backgroundColor: Colors.deepPurpleAccent[200],
+          foregroundColor: Colors.deepOrange[300],
+          child: svg,
+        ),
+      ),
+    ],
+  );
+  Widget line = Text("|", style: TextStyle(fontSize: 16.0));
   Permission permission = Permission.AccessCoarseLocation;
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        RaisedButton(
-          child: Row(
-            children: <Widget>[
-              Icon(Icons.bluetooth),
-              Text("Turn Bluetooth on")
-            ],
-          ),
-          onPressed: () {
-            setState(() {
-              _btOn();
-            });
-          },
-        )
-      ],
-      mainAxisAlignment: MainAxisAlignment.start,
-    );
-  }
-
-  void _btOn() {
-    showDialog(
-        context: context,
-        builder: ((context) => Column(
-              children: <Widget>[
-                Text(
-                  "Turn Bluetooth on",
-                  style: Theme.of(context).textTheme.display1,
-                ),
-                ButtonBar(
-                  children: <Widget>[
-                    FlatButton(
-                      child: Text(
-                        "CANCEL",
-                      ),
-                      onPressed: () {
-                        Navigator.pop(context);
-                      },
+    return Stack(fit: StackFit.expand, children: [
+      Container(
+        color: Colors.red[400],
+        child: Column(
+          children: <Widget>[
+            parolaIcon,
+            Text("Parola", style: Theme.of(context).textTheme.display4),
+            Expanded(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  RaisedButton(
+                    textColor: Colors.white,
+                    color: Colors.red[500],
+                    child: Row(
+                      children: <Widget>[
+                        Icon(FontAwesomeIcons.google),
+                        line,
+                        Text("Sign in with Google"),
+                      ],
                     ),
-                    RaisedButton(
-                        child: Text(
-                          "Turn on",
-                        ),
-                        onPressed: () {
-                          FlutterBlue flutterBlue = FlutterBlue.instance;
-                          final scanSubscription = flutterBlue
-                              .scan(
-                                  scanMode: ScanMode.balanced,
-                                  timeout: Duration(milliseconds: 3000))
-                              .listen((result) {
-                            print("Bluetooth Start Scanning: $result");
-                          }).onError((e) => print("Bluetooth Turned off"));
-                        })
-                  ],
-                )
-              ],
-            )));
+                    onPressed: () {
+                      googleSignIn().then((FirebaseUser user) =>
+                          Navigator.of(context).pushReplacementNamed('/home'));
+                    },
+                  ),
+                  RaisedButton(
+                      textColor: Colors.white,
+                      child: Row(
+                        children: <Widget>[
+                          Icon(FontAwesomeIcons.facebookF),
+                          line,
+                          Text("Sign in with Facebook"),
+                        ],
+                      ),
+                      color: Colors.blue[700],
+                      onPressed: () {
+                        _facebookSignIn();
+                      }),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    ]);
   }
 }
 
-class BeaconRanging extends Model {
-  final beacon = Beacons
-      .ranging(
-        region: BeaconRegionIBeacon(
-          identifier: "test",
-          proximityUUID: "",
-        ),
-        permission: LocationPermission(
-            android: LocationPermissionAndroid.coarse,
-            ios: LocationPermissionIOS.whenInUse),
-        inBackground: false,
-      )
-      .listen((result) {});
-  void _startConnect() {
-    beacon.cancel();
-    notifyListeners();
-  }
-}
+//     return Column(
+//       children: <Widget>[
+//         FloatingActionButton(
+//           elevation: 2.0,
+//           child: Icon(Icons.bluetooth),
+//           onPressed: () {
+//             setState(() {
+//               _btOn();
+//             });
+//           },
+//         )
+//       ],
+//     );
+//   }
+
+//   _btOn() {
+//      showDialog(
+//        context: context,
+//        builder: (context)=>
+//        AlertDialog(
+//       actions: <Widget>[],
+//       title: Text("Bluetooth on"),
+//       content: Column(
+//         children: <Widget>[
+//           Text(
+//             "Turn Bluetooth on",
+//             style: Theme.of(context).textTheme.display1,
+//           ),
+//           ButtonBar(
+//             children: <Widget>[
+//               FlatButton(
+//                 child: Text(
+//                   "CANCEL",
+//                 ),
+//                 onPressed: () {
+//                   Navigator.pop(context);
+//                 },
+//               ),
+//               RaisedButton(
+//                   child: Text(
+//                     "Turn on",
+//                   ),
+//                   onPressed: () {
+//                     _onStart();
+//                   })
+//             ],
+//           )
+//         ],
+//       ),
+//     ));
+
+//   }
+
+//   void _onStart() {
+//     FlutterBlue flutterBlue = FlutterBlue.instance;
+//     final scanSubscription = flutterBlue
+//         .scan(
+//             scanMode: ScanMode.balanced, timeout: Duration(milliseconds: 3000))
+//         .listen((result) {
+//       print("Bluetooth Start Scanning: $result");
+//     });
+//     scanSubscription.cancel();
+//   }
+// }
+
+// class BeaconRanging extends Model {
+//   final beacon = Beacons
+//       .ranging(
+//         region: BeaconRegionIBeacon(
+//           identifier: "test",
+//           proximityUUID: "",
+//         ),
+//         permission: LocationPermission(
+//             android: LocationPermissionAndroid.coarse,
+//             ios: LocationPermissionIOS.whenInUse),
+//         inBackground: false,
+//       )
+//       .listen((result) {});
+//   // void _startConnect() {
+//   //   beacon.cancel();
+//   //   notifyListeners();
+//   // }
+// }
---
+++ no_beacon/README.md
@@ -0,0 +1,8 @@
+# no_beacon
+
+no beacon with google sign in
+
+## Getting Started
+
+For help getting started with Flutter, view our online
+[documentation](https://flutter.io/).
