4116771b5885f0f1e5c82202137753402d5cd149
ram231 <blusea231@gmail.com>
2018-08-02 10:55:02 +0800

    remove some import

--- lib/home.dart
+++ lib/home.dart
@@ -1,7 +1,7 @@
 import 'dart:async';
 
 import 'package:final_parola/main.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+// import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'dart:io';
 // import 'package:firebase_auth/firebase_auth.dart';
