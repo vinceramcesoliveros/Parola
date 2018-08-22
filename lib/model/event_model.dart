20edc3bd9edaf39528efb53284b83cf0771527cb
ram231 <blusea231@gmail.com>
2018-08-22 12:19:45 +0800

    should remove event model.

--- lib/model/event_model.dart
+++ lib/model/event_model.dart
@@ -1,7 +1,6 @@
 import 'package:scoped_model/scoped_model.dart';
 import 'dart:async';
 import 'dart:io';
-import 'package:firebase_storage/firebase_storage.dart';
 import 'package:image_picker/image_picker.dart';
 
 class EventModel extends Model {
